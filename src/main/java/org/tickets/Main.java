67495a9d064b60558a8e5439e6d84d842159124f
Maxim1-1 <Kick52@yandex.ru>
2024-05-01 13:43:01 +0300

    add README.md

--- src/main/java/org/tickets/Main.java
+++ src/main/java/org/tickets/Main.java
@@ -4,30 +4,31 @@ import org.tickets.model.Agent;
 import org.tickets.service.PriceCalculationService;
 import org.tickets.service.TimeCalculationService;
 import org.tickets.utils.JsonUtils;
+
 import java.io.FileWriter;
-import java.nio.file.Files;
-import java.io.File;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.io.IOException;
 import java.util.*;
 
 
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            String json = Files.readString(new File("src/main/resources/tickets.json").toPath());
+        try (InputStream inputStream = Main.class.getResourceAsStream("/tickets.json")){
+
+            String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
             List<Agent> agents = JsonUtils.convertJsonToAgents(json);
 
             PriceCalculationService priceCalculationService = new PriceCalculationService(agents);
             TimeCalculationService timeCalculationService = new TimeCalculationService(agents);
 
             double differenceMedianAveragePrice = priceCalculationService.calculateDifferenceMedianAveragePrice();
-            Map<String,String> minimalTimeFlight = timeCalculationService.calculationMinimumFlightTime();
+            Map<String, String> minimalTimeFlight = timeCalculationService.calculationMinimumFlightTime();
 
-            StringBuilder resultData = new StringBuilder(String.format("Разница между средней ценой и медианой = %s  \n",differenceMedianAveragePrice));
+            StringBuilder resultData = new StringBuilder(String.format("Разница между средней ценой и медианой = %s  \n", differenceMedianAveragePrice));
 
             minimalTimeFlight.forEach((key, value) -> {
-                resultData.append(String.format("Минимальное время полета через компанию %s , составляет : %s \n",key,value));
+                resultData.append(String.format("Минимальное время полета через компанию %s , составляет : %s \n", key, value));
             });
 
             writeDataInFile(resultData.toString());
@@ -38,14 +39,13 @@ public class Main {
     }
 
 
-    public static void writeDataInFile (String data){
-        try (FileWriter writer = new FileWriter("result.txt");) {
+    public static void writeDataInFile(String data) {
+        String filePath = System.getProperty("user.dir") + "/../result.txt";
+        try (FileWriter writer = new FileWriter(filePath);) {
             writer.write(data);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
-
-
 }
